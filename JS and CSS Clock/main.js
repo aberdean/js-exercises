466532c89eb5d210e0d7d7403f7988cdbc2362c6
Antonella Bernobich Dean <aberdean@users.noreply.github.com>
2018-08-11 10:11:21 +0200

    Finalize clock functionality

--- JS and CSS Clock/main.js
+++ JS and CSS Clock/main.js
@@ -6,15 +6,16 @@ const setDate = () => {
   const now = new Date();
 
   const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  const secondsDegrees = ((seconds * 360) / 60) + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
   const minutes = now.getMinutes();
-  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  const minutesDegrees = ((minutes * 360) / 60) + 90;
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
   const hours = now.getHours();
-  const hoursDegrees = ((hours / 12) * 360) + 90;
+  /** Ensure smooth transition of the hour hand */
+  const hoursDegrees = (((hours * 360) / 12) + (minutes / 2)) + 90;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 
 }
